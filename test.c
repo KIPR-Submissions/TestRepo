70ea7b5900725114f77b307fddc16a7d69b8b324
KIPR-Submissions <46693830+KIPR-Submissions@users.noreply.github.com>
2019-03-15 14:03:34 -0500

    test .c file
    test

---
+++ test.c
@@ -0,0 +1,18 @@
+
+#include <kipr/botball.h>
+
+//This program was created on the Wallaby. It will be used to test uploading
+//to GitHub and making changes via multiple users.
+
+int main()
+{
+    printf("Hello, my name is Ashley \n");
+    //Do nothing right now
+
+    //This chunk of code added through GitHub edit.
+    motor(0,90);
+    motor(3,89);
+    msleep(3000);
+    //test comment
+    return 0;
+}
